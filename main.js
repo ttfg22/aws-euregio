f6e3e5c9a051d48352eb680a55f692d831d6a218
Tashi Freyer <tashi.freyer@student.uibk.ac.at>
2023-05-11 16:22:07 +0200

    added new function

--- main.js
+++ main.js
@@ -13,7 +13,8 @@ let map = L.map("map", {
 
 // thematische Layer
 let themaLayer = {
-    stations: L.featureGroup()
+    stations: L.featureGroup(),
+    temperature: L.featureGroup()
 }
 
 // Hintergrundlayer
@@ -21,13 +22,14 @@ let layerControl = L.control.layers({
     "Relief avalanche.report": L.tileLayer(
         "https://static.avalanche.report/tms/{z}/{x}/{y}.webp", {
         attribution: `© <a href="https://lawinen.report">CC BY avalanche.report</a>`,
-        maxZoom:12,
+        maxZoom: 12,
     }).addTo(map),
     "Openstreetmap": L.tileLayer.provider("OpenStreetMap.Mapnik"),
     "Esri WorldTopoMap": L.tileLayer.provider("Esri.WorldTopoMap"),
     "Esri WorldImagery": L.tileLayer.provider("Esri.WorldImagery")
 }, {
-    "Wetterstationen": themaLayer.stations.addTo(map)
+    "Wetterstationen": themaLayer.stations.addTo(map),
+    "Temperatur": themaLayer.temperature.addTo(map)
 }).addTo(map);
 
 // Maßstab
@@ -35,13 +37,8 @@ L.control.scale({
     imperial: false,
 }).addTo(map);
 
-// Darstellung der Wetterstationen
-async function showStations(url) {
-    let response = await fetch(url);
-    let jsondata = await response.json()
-
-    // Wetterstationen mit Icons und Popups implementieren
-    // PROBLEM: Um eine Rundung der Werte der Windgeschwindigkeit zu erreichen, wurde die FixedTo() Funktion eingebaut. Wenn keine Messwerte vorhanden sind, wird jedoch 'NaN' angezeigt, anstatt der festgelegten Fehlermeldung 
+// Funktion, die Wetterstationen mit Icons und Popups implemetiert
+function writeStationLayer(jsondata) {
     L.geoJSON(jsondata, {
         pointToLayer: function (feature, latlng) {
             return L.marker(latlng, {
@@ -57,24 +54,31 @@ async function showStations(url) {
             let mas = feature.geometry.coordinates[2]
             let date_time = new Date(prop.date);
             let WG;
-            if(prop.WG){
-                WG=(prop.WG*3.6).toFixed(1);
-            }else{
-                WG="-";
+            if (prop.WG) {
+                WG = (prop.WG * 3.6).toFixed(1);
+            } else {
+                WG = "-";
             }
             //let WG = (prop.WG)?(prop.WG*3.6).toFixed(1):"-";
             console.log(mas)
             layer.bindPopup(`
-                <h1>${prop.name}, ${mas} m ü.A. </h1><ul>
-                    <li>Lufttemperatur (Grad °): ${prop.LT || "-"} </li>
-                    <li>relative Luftfeuchte (%): ${prop.RH || "-"} </li>
-                    <li>Windgeschwindigkeit (km/h): ${WG}</li>
-                    <li>Schneehöhe (cm): ${prop.HS || "-"}</li>
-                </ul></>
-                <!-- -->
-                <span>${date_time.toLocaleString()}</span>
-            `);
+            <h1>${prop.name}, ${mas} m ü.A. </h1><ul>
+                <li>Lufttemperatur (Grad °): ${prop.LT || "-"} </li>
+                <li>relative Luftfeuchte (%): ${prop.RH || "-"} </li>
+                <li>Windgeschwindigkeit (km/h): ${WG}</li>
+                <li>Schneehöhe (cm): ${prop.HS || "-"}</li>
+            </ul></>
+            <span>${date_time.toLocaleString()}</span>
+        `);
         }
     }).addTo(themaLayer.stations)
 }
-showStations("https://static.avalanche.report/weather_stations/stations.geojson");
+
+// Darstellung der Wetterstationen
+async function loadStations(url) {
+    let response = await fetch(url);
+    let jsondata = await response.json()
+    writeStationLayer(jsondata)
+}
+
+loadStations("https://static.avalanche.report/weather_stations/stations.geojson");
