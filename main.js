e655175a50496238a36746d44c301a95025e6572
Tashi Freyer <tashi.freyer@student.uibk.ac.at>
2023-05-11 16:07:43 +0200

    added version of if clauses as comment

--- main.js
+++ main.js
@@ -56,15 +56,22 @@ async function showStations(url) {
             let prop = feature.properties;
             let mas = feature.geometry.coordinates[2]
             let date_time = new Date(prop.date);
-            //let WG = if(prop.WG){return(prop.WG*3.6).toFixed(1)}else{return("keine Messungen vorhanden")}
+            let WG;
+            if(prop.WG){
+                WG=(prop.WG*3.6).toFixed(1);
+            }else{
+                WG="-";
+            }
+            //let WG = (prop.WG)?(prop.WG*3.6).toFixed(1):"-";
             console.log(mas)
             layer.bindPopup(`
                 <h1>${prop.name}, ${mas} m ü.A. </h1><ul>
-                    <li>Lufttemperatur (Grad °): ${prop.LT || "keine Messungen vorhanden"} </li>
-                    <li>relative Luftfeuchte (%): ${prop.RH || "keine Messungen vorhanden"} </li>
-                    <li>Windgeschwindigkeit (km/h): ${Number(prop.WG*3.6).toFixed(2)|| "keine Messungen vorhanden"}</li>
-                    <li>Schneehöhe (cm): ${prop.HS || "keine Messungen vorhanden"}</li>
+                    <li>Lufttemperatur (Grad °): ${prop.LT || "-"} </li>
+                    <li>relative Luftfeuchte (%): ${prop.RH || "-"} </li>
+                    <li>Windgeschwindigkeit (km/h): ${WG}</li>
+                    <li>Schneehöhe (cm): ${prop.HS || "-"}</li>
                 </ul></>
+                <!-- -->
                 <span>${date_time.toLocaleString()}</span>
             `);
         }
